f961ead3cb7fde04f67aa838573a654e7e3f8a4a
tomasz <tomaszkacperkulig@gmail.com>
2022-04-29 21:23:51 +0200

    fix

--- contracts/UniswapV2Library.sol
+++ contracts/UniswapV2Library.sol
@@ -52,12 +52,12 @@ library UniswapV2Library {
     }
 
     // performs chained getAmountOut calculations on any number of pairs
-    function getAmountsOut(address[] memory factory, uint amountIn, address[] memory path) internal view returns (uint[] memory amounts) {
+    function getAmountsOut(address[] memory pools, uint amountIn, address[] memory path) internal view returns (uint[] memory amounts) {
         require(path.length >= 2, 'UniswapV2Library: INVALID_PATH');
         amounts = new uint[](path.length);
         amounts[0] = amountIn;
         for (uint i; i < path.length - 1; i++) {
-            (uint reserveIn, uint reserveOut) = getReserves(factory[i], path[i], path[i + 1]);
+            (uint reserveIn, uint reserveOut) = getReserves(pools[i], path[i], path[i + 1]);
             amounts[i + 1] = getAmountOut(amounts[i], reserveIn, reserveOut);
         }
     }
